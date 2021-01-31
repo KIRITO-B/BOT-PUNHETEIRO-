a20cd5d6ccf4b12be96c7cf2d4ce90ac104349ab
KIRITO-B <78248834+KIRITO-B@users.noreply.github.com>
2021-01-30 22:44:13 -0300

    Add files via upload

---
+++ orc/help.js
@@ -0,0 +1,144 @@
+const help = (prefix) => {
+	return `
+
+             ⚡𝐌𝐄𝐍𝐔 ⚡
+
+╠════✪〘 FIGURINHA 〙✪═════
+║
+║
+╠➣ COMANDO: ${prefix}sticker
+╠➣ DESC: Converter foto/gif/video em figurinha
+╠➣ USO: basta enviar o comando\n
+║
+║
+╠➣ COMANDO: ${prefix}imagem
+╠➣ DESC: Converte a figurinha em imagem
+╠➣USO: Responda a figurinha com o comando\n
+║
+║
+╠═════✪〘 MEME 〙✪══════
+║
+║
+╠➣ COMANDO: ${prefix}meme
+╠➣ DESC: Imagens aleatórias de memes [Inglês]
+╠➣ USO: Apenas envie o comando\n
+║
+║
+╠➣ COMANDO: ${prefix}memeindo
+╠➣ DESC: Imagens aleatórias de memes da Indonésia
+╠➣ USO: Apenas envie o comando\n
+║
+║
+╠═════✪〘 FUNNY :) 〙✪═════
+║ 
+║
+╠➣ COMANDO: ${prefix}loli
+╠➣ DESC: Imagens de loli
+╠➣ USO: Apenas envie o comando\n
+║
+║
+╠➣ COMANDO: ${prefix}loli18
+╠➣ DESC: Imagens +18 de lolis
+╠➣ USO: Apenas envie o comando\n
+╠➣ NOTA: Disponivel\n
+║
+║
+╠➣ COMANDO: ${prefix}simi
+╠➣ DESC: Sua mensagem será respondida por simi
+╠➣ USO: ${prefix}simi [seu texto]\n
+║
+║
+╠➣ COMANDO: ${prefix}wait
+╠➣ DESC: Saiba sobre o anime [Que anime é]
+╠➣ USO: Envie imagem do anime com o comando\n
+║
+║
+╠➣ COMANDO: ${prefix}listakamikaze
+╠➣ DESC: Mostra todos os numeros bloqueados do bot
+╠➣ USO: Apenas envie o comando\n
+║
+║
+╠═════✪⚡〘 GRUPO 〙⚡✪═════
+║➣     ⚡COMANDOS ADMINS⚡
+║
+╠➣ COMANDO: ${prefix}add
+╠➣ DESC: Adicionar membro ao grupo
+╠➣ USO: ${prefix}add 55xxxxxxxx\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+║
+╠➣ COMANDO: ${prefix}banir
+╠➣ DESC: Banir membros do grupo
+╠➣ USO: ${prefix}kick @tag do membro\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║➣ OBS: Não faça o bot remover duas pessoas ao mesmo tempo!\n
+║
+║
+╠➣ COMANDO: ${prefix}promover
+╠➣ DESC: Promove um membro a ser admin
+╠➣ USO: ${prefix}promover @tag do membro\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+║
+╠➣ COMANDO: ${prefix}rebaixar
+╠➣ DESC: Rebaixa um admin a ser membro comum
+╠➣ USO: ${prefix}rebaixar @tag do admin\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+║
+╠➣ COMANDO: ${prefix}linkgrupo
+╠➣ DESC: Obtenha o link do grupo
+╠➣ USO: Apenas envie o comando\n
+╠➣ NOTA: O bot precisa ser um admin!\n
+║
+║
+╠➣ COMANDO: ${prefix}chamada
+╠➣ DESC: Marca todos os membros do grupo
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Você precisa ser um admin!\n
+║
+║
+╠➣ COMANDO: ${prefix}simih
+╠➣ DESC: Ative o modo simi no grupo
+╠➣ ATIVAR: ${prefix}simih 1 para ativar 
+╠➣ DESATIVAR: ${prefix}simih 0 para desativar 
+╠➣ NOTA: Usado somente por admin\n
+║
+║
+╠➣ COMANDO: ${prefix}bemvindo
+╠➣ DESC: Ativar mensagens de boas vindas no grupo
+╠➣ USO: ${prefix}bemvindo 1 para ativar e 0 para desativar
+╠➣ NOTA: Apenas envie o comando\n
+║
+║
+╠═════✪〘 OUTROS COMANDOS 〙✪═════ 
+║   
+║
+╠➣ COMANDO: ${prefix}limparchat
+╠➣ DESC: Limpa todas as conversas do seu bot
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Comando exclusivo do dono do bot
+║
+║
+╠➣ COMANDO: ${prefix}autoban
+╠➣ DESC: Faz o bot sair imediatamente do grupo
+╠➣ USO: Apenas envie o comando
+╠➣ NOTA: Comando exclusivo do dono do bot
+║
+║
+╠➣ COMANDO: *${prefix}mudar prefixo
+╠➣ DESC: Substitui o prefixo do bot
+╠➣ USO: ${prefix}setprefix [texto|opcional]\n
+╠➣ NOTA: Comando exclusivo do dono do bot
+║
+║
+╠════════════════════
+║
+║🍁𝐃𝐎𝐍𝐎 𝐃𝐎 𝐁𝐎𝐓 :𝑲𝑰𝑹𝑰𝑻𝑶 𝑶𝑭 𝑺𝑨𝑫ッ(gostozim)  
+║
+║🍁𝐂𝐎𝐍𝐓𝐀𝐓𝐎 :Wa.me/+551199179-6330
+╚════════════════════`
+
+}
+
+exports.help = help
