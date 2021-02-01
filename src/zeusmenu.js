25aca372bbce7c617739b76de6f5f8e3e0b2ed09
KIRITO-B <78248834+KIRITO-B@users.noreply.github.com>
2021-02-01 20:30:06 -0300

    Add files via upload

---
+++ src/zeusmenu.js
@@ -0,0 +1,43 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos da ❁𝑨𝑺𝑼𝑵𝑨-𝑺𝑨𝑵❁:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  FEITO POR *𝑲𝑰𝑹𝑰𝑻𝑶 𝑶𝑭 𝑺𝑨𝑫ッ*
+  DUVIDAS? 👇
+  WA.me/5511991796330
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
+
+
+
